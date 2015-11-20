aedb0d1ef0075552c9c16f3a0bf04a62c16ac1fb
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2015-11-20 19:31:13 +0100

    Quite advanced, missing example cpp files and more CMakeLists.txt editing

--- config/setup.py
+++ config/setup.py
@@ -7,7 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['package_name'],
     package_dir={'': 'src'},
-    requires=[requires_place_holder]
+    requires=requires_place_holder
 )
 
 setup(**setup_args)
